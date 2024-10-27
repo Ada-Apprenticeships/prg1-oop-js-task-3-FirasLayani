c5b110a35aa7e07c7ed3e294a1db2a49ff1f042e
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-27 22:12:59 +0000

    Created functions for validating integers and priority, and for retreiving today's data (incorrect format)

--- oop.js
+++ oop.js
@@ -1,13 +1,13 @@
 PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
-
+// 'Broken'
 function validInteger (value) { // value can be a string or a number (integer)
-  
+  return value%1===0 && value >= 0 ? true : false
 }  
 
 
 function validatePriority(priority) { // value can be a string or a number (integer)
-  
+  return [1,3,5,7].includes(Number(priority)) ? Number(priority) : 1
 }
 
 
@@ -27,9 +27,10 @@ class ToDo {
 }
 
 
+// Testing
 
-
-
+console.log(validInteger('4'))
+console.log(validatePriority(3))
 
 
 
