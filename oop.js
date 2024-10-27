d179d7a38cb2f6da26a2d120ff237be26d559355
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-27 22:35:56 +0000

    Refactored todaysDate and formatDatePart functions for readability

--- oop.js
+++ oop.js
@@ -2,29 +2,28 @@ PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 // 'Broken'
 function validInteger (value) { // value can be a string or a number (integer)
-  return value%1===0 && value >= 0 ? true : false
+  return value%1===0 && value >= 0 ? true : false;
 }  
 
 
 function validatePriority(priority) { // value can be a string or a number (integer)
-  return [1,3,5,7].includes(Number(priority)) ? Number(priority) : 1
+  return [1,3,5,7].includes(Number(priority)) ? Number(priority) : 1;
 }
 
 function formatDatePart(part) {
-  return String(part).length !== 2 ? `0${part}` : part
+  return part < 10 ? `0${part}` : part;
 }
 
 function todaysDate () {
-  const now = new Date()
-  const day = now.getDate();
-  const month = now.getMonth()+1;
+  const now = new Date();
+  const day = formatDatePart(now.getDate());
+  const month = formatDatePart(now.getMonth()+1);
   const year = now.getFullYear();
-  const hour = now.getHours();
-  const minute = now.getMinutes();
-  const second = now.getSeconds();
-  return `${formatDatePart(day)}/${formatDatePart(month)}/${year} ${formatDatePart(hour)}:${formatDatePart(minute)}:${formatDatePart(second)}`
+  const hour = formatDatePart(now.getHours());
+  const minute = formatDatePart(now.getMinutes());
+  const second = formatDatePart(now.getSeconds());
+  return `${day}/${month}/${year} ${hour}:${minute}:${second}`
 }
-
 
 class Task  {
 
